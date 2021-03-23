405ef7edbf6b906560665365b1f70e25985a0517
totinIstvan <totin.istvan@gmail.com>
2021-03-23 20:28:22 +0100

    Complete task

--- src/main/java/com/codecool/whattoprint/Challenge.java
+++ src/main/java/com/codecool/whattoprint/Challenge.java
@@ -2,6 +2,18 @@ package com.codecool.whattoprint;
 
 class Challenge {
     public static String decideAndSay(String[] words, int number) {
-        return words[0];
+        String res = "";
+        if (number == 0 || words.length < 2) {
+            return res;
+        }
+
+        if (number % 7 == 0 && number % 3 == 0) {
+            res = words[0] + words[1];
+        } else if (number % 7 == 0) {
+            res = words[1];
+        } else if (number % 3 == 0) {
+            res = words[0];
+        }
+        return res;
     }
 }
